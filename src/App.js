617f4edb4e9b0887caa49ba5b4d2b4dd5ff25f40
the-arthur <feeblezzz@gmail.com>
2022-02-02 13:32:27 +0300

    some progress in learning

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
-
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import './styles/app.css';
 
 function App() {
 
-  const [value, setValue,] = useState('Текс в инпуте')
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'JS это язык програмирования' },
+    { id: 2, title: 'JavaScript 2', body: 'JS это язык програмирования' },
+    { id: 3, title: 'JavaScript 3', body: 'JS это язык програмирования' }
+  ])
 
-
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Python', body: 'JS это язык програмирования' },
+    { id: 2, title: 'Python 2', body: 'JS это язык програмирования' },
+    { id: 3, title: 'Python 3', body: 'JS это язык програмирования' }
+  ])
 
   return (
     <div className="App">
-      <Counter />
+      <PostList posts={posts} title="Список постов 1" />
+      <PostList posts={posts2} title="Список постов 1" />
+      < Counter />
       <ClassCounter />
     </div>
   );
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+const PostItem = (props) => {
+    return (
+        <div className="post">
+            <div className="post-content">
+                <strong>
+                    {props.post.id}. {props.post.title}
+                </strong>
+                <div>
+                    {props.post.body}
+                </div>
+            </div>
+            <div className="post-btns">
+                <button className="post-btn">Удалить</button>
+            </div>
+        </div>
+    )
+};
+
+export default PostItem;
