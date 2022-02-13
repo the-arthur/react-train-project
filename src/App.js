8683f6b791d4048644454cc64614274cac822368
the-arthur <feeblezzz@gmail.com>
2022-02-13 23:24:36 +0300

    custom hooks, useMemo, some progress toDo list

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useMemo } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
+import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyModal from './components/UI/myModal/MyModal';
+import { usePosts } from './hooks/usePosts';
 import './styles/app.css';
 
 function App() {
 
   const [posts, setPosts] = useState([
     { id: 1, title: 'JavaScript', body: 'JS это язык програмирования' },
-    { id: 2, title: 'JavaScript 2', body: 'JS это язык програмирования' },
-    { id: 3, title: 'JavaScript 3', body: 'JS это язык програмирования' }
+    { id: 2, title: 'Python', body: 'dwadawdawda' },
+    { id: 3, title: 'Script', body: 'ания' }
   ])
-
+  const [filter, setFilter] = useState({ sort: '', query: '' })
+  const [modal, setModal] = useState()
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
-
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
   return (
     <div className="App">
-      <PostForm create={createPost} />
-      <PostList remove={removePost} posts={posts} title="Список постов 1" />
-      < Counter />
+      <MyButton style={{ marginTop: "30px" }} onClick={() => { setModal(true) }}>
+        Создать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal} >
+        <PostForm create={createPost} />
+      </MyModal>
+      <PostFilter filter={filter} setFilter={setFilter} />
+      <hr style={{ marginTop: "15px" }} />
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов 1" />
+      <Counter />
       <ClassCounter />
     </div>
   );
